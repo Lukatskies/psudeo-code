c3628d79f8551eec1d21f9516118f64cc6f05037
LUKE SHEESLEY <Luke.sheesley@uvm.edu>
2024-09-30 10:56:56 -0400

    first commit

---
+++ Index.js
@@ -0,0 +1,58 @@
+// Establish who is player one and two (Left and Right side)
+
+
+// left paddle speed
+    
+
+// right paddle speed
+
+
+// puck speed
+
+
+// move paddles by their speed
+
+
+// move ball by its speed
+
+
+// keep track of when need to reset the ball position
+
+
+// prevent ball from going through walls
+
+
+// game loop
+
+
+// start puck on the right side
+
+
+// start puck on the left side
+
+
+// reset ball if it goes past right or left paddle
+
+
+// if ball goes past left paddle, reset on right side
+
+
+// if ball goes past right paddle, reset on left side
+
+
+// draw ball
+
+
+// draw walls
+
+
+// wall boundries
+
+
+// up arrow key
+
+
+// down arrow key
+
+
+// start game
